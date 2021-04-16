eee198b15fcb23fb5c247bf31973e18e575a255f
ang-sant-abril <ang-sant-abril@gmail.com>
2021-04-16 07:27:25 +0000

    compatibilidad IE

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -14,6 +14,8 @@
  * Learn more in https://angular.io/guide/browser-support
  */
 
+import 'core-js'; // npm i core-js --save
+
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
